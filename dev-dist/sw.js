0fd54b0a04cc006c8a42e5b9840dac13c2d00089
ma-shamshiri <ma.shamshiri@gmail.com>
2025-04-06 16:32:54 -0400

    Modify the video file path

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0mjidfa4kto"
+    "revision": "0.g5acab7d2hg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/InstagramGallery/data.ts
+++ src/components/InstagramGallery/data.ts
@@ -1,18 +1,4 @@
-import {
-    azita_mohammadkarimi_teaser,
-    nasim_biyarjomandy_teaser,
-    farzaneh_shahrtash_teaser,
-    shadi_azizi_teaser,
-    mostafa_naghipourfar_teaser,
-    vahid_rajabloo_teaser,
-    nasim_biyarjomandy_cover,
-    mostafa_naghipourfar_cover,
-    vahid_rajabloo_cover,
-    farzaneh_shahrtash_cover,
-    shadi_azizi_cover,
-    azita_mohammadkarimi_cover
-
-} from "../../assets";
+import { azita_mohammadkarimi_cover, farzaneh_shahrtash_cover, mostafa_naghipourfar_cover, nasim_biyarjomandy_cover, shadi_azizi_cover, vahid_rajabloo_cover } from "../../assets";
 
 export interface InstagramPost {
     id: string;
@@ -29,42 +15,42 @@ export const instagramPosts: InstagramPost[] = [
         imageUrl: shadi_azizi_cover,
         caption: "نوآوری",
         // link: "https://www.instagram.com/tedxuniversityoftehran/",
-        videoUrl: shadi_azizi_teaser,
+        videoUrl: "shadi_azizi_teaser.mp4",
     },
     {
         id: "1",
         imageUrl: azita_mohammadkarimi_cover,
         caption: "تیم TEDx دانشگاه تهران",
         // link: "https://www.instagram.com/tedxuniversityoftehran/",
-        videoUrl: azita_mohammadkarimi_teaser,
+        videoUrl: "azita_mohammadkarimi_teaser.mp4",
     },
     {
         id: "2",
         imageUrl: nasim_biyarjomandy_cover,
         caption: "ایده‌های نوآورانه",
         // link: "https://www.instagram.com/tedxuniversityoftehran/",
-        videoUrl: nasim_biyarjomandy_teaser,
+        videoUrl: "nasim_biyarjomandy_teaser.mp4",
     },
     {
         id: "3",
         imageUrl: mostafa_naghipourfar_cover,
         caption: "همایش TEDx",
         // link: "https://www.instagram.com/tedxuniversityoftehran/",
-        videoUrl: mostafa_naghipourfar_teaser,
+        videoUrl: "mostafa_naghipourfar_teaser.mp4",
     },
     {
         id: "4",
         imageUrl: vahid_rajabloo_cover,
         caption: "الهام‌بخش",
         // link: "https://www.instagram.com/tedxuniversityoftehran/",
-        videoUrl: vahid_rajabloo_teaser,
+        videoUrl: "vahid_rajabloo_teaser.mp4",
     },
     {
         id: "5",
         imageUrl: farzaneh_shahrtash_cover,
         caption: "نوآوری",
         // link: "https://www.instagram.com/tedxuniversityoftehran/",
-        videoUrl: farzaneh_shahrtash_teaser,
+        videoUrl: "farzaneh_shahrtash_teaser.mp4",
     },
 
 ];
